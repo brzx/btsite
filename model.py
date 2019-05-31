c5c0a44e2aadf1cb8381871733206761819545d7
Brian Zhu <brzx@foxmail.com>
2019-05-31 14:10:57 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 
 from sqlalchemy import (
     Column, Integer, Sequence, String, DateTime, 
+    Float, DECIMAL, ForeignKey, 
 )
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
@@ -24,5 +26,43 @@ class AssetType(Base):
     isdeleted = Column(String(2), default='0')
     create_dt = Column(DateTime(), default=datetime.now)
     update_dt = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    children = relationship('Asset')
+
     def __repr__(self):
         return "<AssetType('%s', '%s')>" % (self.assetname, self.detailtype)
+
+class Asset(Base):
+    __tablename__ = 'asset'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    asiden = Column(String(200), nullable=False)
+    asorgan = Column(String(200), nullable=False)
+    bill_dt = Column(String(50))
+    repayment_dt = Column(String(50))
+    credit_limit = Column(String(50))
+    year_rate = Column(String(50))
+    comment = Column(String(200))
+    isdeleted = Column(String(2), default='0')
+    create_dt = Column(DateTime(), default=datetime.now)
+    update_dt = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    assettype_id = Column(Integer, ForeignKey('assettype.id'))
+
+    children = relationship('AssetRecord')
+
+    def __repr__(self):
+        return "<Asset('%s', '%s')>" % (self.asiden, self.asorgan)
+
+class AssetRecord(Base):
+    __tablename__ = 'assetrecord'
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    amount = Column(Float, nullable=False)
+    comment = Column(String(200))
+    isdeleted = Column(String(2), default='0')
+    create_dt = Column(DateTime(), default=datetime.now)
+    update_dt = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+
+    asset_id = Column(Integer, ForeignKey('asset.id'))
+
+    def __repr__(self):
+        return "<AssetRecord('%s', '%s')>" % (self.asiden, self.asorgan)
